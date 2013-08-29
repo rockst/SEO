6ac1f428a12e5df180a6884557eebcb8834de4c3
rockst <rockst@gmail.com>
2013-08-29 17:05:37 +0800

    增加家族, 會員 與自制內容

--- dom/.config.php
+++ dom/.config.php
@@ -1,8 +1,8 @@
 <?php
 	Define('XMLROOT', dirname(__FILE__) . '/output/'); // 放 XML 實體檔案的位置
 	Define('GZROOT', dirname(__FILE__) . '/output/'); // 放 gzip 實體檔案的位置
-	Define('MEMBERPATH', dirname(__FILE__) . '/member/'); // 放會員 xml, gzip 實體檔案的位置
-	Define('VENDORPATH', dirname(__FILE__) . '/vendor/'); // 放廠商 xml, gzip 實體檔案的位置
-	Define("URLLIMIT", 50000); // 每個 XML 檔案最大的 URL 數
-	Define("XMLSIZE", 10485760); // 每個 gzip 所能接受的最大檔案容量
+	Define('MEMBERPATH', dirname(__FILE__) . '/member/');
+	Define('VENDORPATH', dirname(__FILE__) . '/vendor/');
+	Define("URLLIMIT", 50000);
+	Define("XMLSIZE", 10485760);
 ?>
---
+++ dom/member/family.php
@@ -0,0 +1,9 @@
+<?php
+	include('simple_html_dom.php');
+
+	$rows = array(
+		array('url'=>'http://verywed.com/family/?o=hot&p=1', 'page'=>'p=16', 'pattern'=>'div.img a'),
+	);
+	
+	getURL($rows);
+?>
