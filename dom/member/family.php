9b00dd06001e6b2f09c7ea6b70b88f932bb4f259
rockst <rockst@gmail.com>
2013-08-30 14:32:18 +0800

    remove

--- dom/member/family.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('simple_html_dom.php');
-
-	$rows = array(
-		array('url'=>'http://verywed.com/family/?o=hot&p=1', 'page'=>'p=16', 'pattern'=>'div.img a'),
-	);
-	
-	getURL($rows);
-?>
